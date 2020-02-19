5c526df1ccba3da2283bb581ce7aa56ea229efdd
porawiec <54656651+porawiec@users.noreply.github.com>
2020-02-19 10:42:41 -0600

    changed cors settings

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module Mod4PilotProject
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
   end
