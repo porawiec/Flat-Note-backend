a5b3b0d7dc9032526cdeec522b146ab71ede4886
porawiec <54656651+porawiec@users.noreply.github.com>
2020-03-12 19:50:42 -0500

    added routes for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,6 +9,30 @@ class NotesController < ApplicationController
         render json: notes, include: [:user]
     end
 
-    
+    def create
+        @note = Note.new(user_id: params[user_id], title: params[:title], description: params[:description])
+        if note.valid?
+            note.save
+            render json: note
+        else
+            render json: {error: 'Note could not be created'}
+    end
+
+    def update
+        note = Note.find(params[:id])
+        if note.valid?
+            note.update(title: params[:title], description: params[:description])
+            render json: note
+        else
+            render json: {error: 'Note could not be updated'}
+    end
+
+    def destroy
+        note = Note.find(params[:id])
+        if note.valid?
+            note.delete
+        else
+            render json: {error: 'Note could not be deleted'}
+    end
     
 end
